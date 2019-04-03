9fc710a5e7984f58a8a4d7ea8636889ab25ed55d
jiningEcplise <969825171@qq.com>
2019-04-03 21:55:27 +0800

    初始版本

---
+++ src/main/java/com/_520it/pss/HelloServlet.java
@@ -0,0 +1,24 @@
+package com._520it.pss;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("hello world");
+		req.setAttribute("jining", "hello world");
+		req.getRequestDispatcher("hello.jsp").forward(req, resp);
+	}
+
+}
---
+++ src/test/java/com/_520it/test/TestApp.java
@@ -0,0 +1,12 @@
+package com._520it.test;
+
+import org.junit.Test;
+
+public class TestApp {
+	@Test
+	public void test() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hello world");
+	}
+
+}
