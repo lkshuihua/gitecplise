643f7d14b44e9be8cc2e7ddcdbac5846e5594c2e
lkshuihua <lkshuihua_2014@163.com>
2019-04-03 23:25:53 +0800

    edit by idea

--- src/test/java/com/_520it/test/Hello.java
+++ src/test/java/com/_520it/test/Hello.java
@@ -3,5 +3,6 @@ package com._520it.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("idea");
     }
 }
